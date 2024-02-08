97c2a926edacd119610e3b9bd16eda1c549009ee
Samuel Shen <sshen05@terpmail.umd.edu>
2024-02-08 09:57:02 -0500

    Finish ex2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [query, setQuery] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/persons').then((response) => {
+            setPersons(response.data);
+        });
+    }, []);
 
     const handleFilterOnChange = (e) => {
         setQuery(e.target.value.toLowerCase());
@@ -60,7 +62,7 @@ const App = () => {
                 onNumberChange={handleOnNumberChange}
             />
             <h2>Numbers</h2>
-			<Persons persons={personsToShow} />
+            <Persons persons={personsToShow} />
         </div>
     );
 };
