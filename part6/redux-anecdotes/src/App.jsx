8ba61b2cde8b99c0572b91deb48065aae77cf914
Samuel Shen <sshen05@terpmail.umd.edu>
2024-03-18 13:04:23 -0400

    Finish ex6.8

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,29 +1,11 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { vote as countVote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector((state) => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(countVote(id))
-  }
-
-  const sortedAnecdotes = anecdotes.toSorted((anecdote1, anecdote2) => anecdote2.votes - anecdote1.votes)
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {sortedAnecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types'
+import { useSelector, useDispatch } from 'react-redux'
+import { vote as countVote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, handleClick }) => (
+  <div>
+    <div>{anecdote.content}</div>
+    <div>
+      has {anecdote.votes}
+      <button onClick={handleClick}>vote</button>
+    </div>
+  </div>
+)
+
+Anecdote.propTypes = {
+  anecdote: PropTypes.shape({
+    content: PropTypes.string.isRequired,
+    votes: PropTypes.number.isRequired,
+  }).isRequired,
+  handleClick: PropTypes.func.isRequired,
+}
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(countVote(id))
+  }
+
+  const sortedAnecdotes = anecdotes.toSorted((anecdote1, anecdote2) => anecdote2.votes - anecdote1.votes)
+
+  return (
+    <div>
+      {sortedAnecdotes.map((anecdote) => (
+        <Anecdote key={anecdote.id} anecdote={anecdote} handleClick={() => vote(anecdote.id)} />
+      ))}
+    </div>
+  )
+}
+
+export default AnecdoteList
