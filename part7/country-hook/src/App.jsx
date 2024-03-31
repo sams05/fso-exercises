25d7cf41b7fe7f631f6bda508420f99c9db40d1d
Samuel Shen <sshen05@terpmail.umd.edu>
2024-03-31 19:20:35 -0400

    Finish ex7.7

--- part7/country-hook/src/App.jsx
+++ part7/country-hook/src/App.jsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
+
+const BASE_URL = 'https://studies.cs.helsinki.fi/restcountries/api/name/'
 
 const useField = (type) => {
   const [value, setValue] = useState('')
@@ -11,14 +13,32 @@ const useField = (type) => {
   return {
     type,
     value,
-    onChange
+    onChange,
   }
 }
 
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = (await axios.get(`${BASE_URL}/${name}`)).data
+
+        setCountry({
+          found: true,
+          data: {
+            name: data.name.common,
+            capital: data.capital[0],
+            population: data.population,
+            flag: data.flags.svg
+          }
+        })
+      } catch (error) {
+        setCountry({ found: false })
+      }
+    })()
+  }, [name])
 
   return country
 }
@@ -29,19 +49,15 @@ const Country = ({ country }) => {
   }
 
   if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div>not found...</div>
   }
 
   return (
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img src={country.data.flag} height="100" alt={`flag of ${country.data.name}`} />
     </div>
   )
 }
